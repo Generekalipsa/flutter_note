b9c173936708ed66279060ca60a06caeb4c5b9af
Generekalipsa <djgamez1222@gmail.com>
2024-01-23 20:06:47 +0100

    Adding persistance via shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Note',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyanAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Note'),
@@ -32,6 +33,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Map<String, String>> _notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNotes();
+  }
+
+  void _loadNotes() async {
+    final prefs = await SharedPreferences.getInstance();
+    final notesJson = prefs.getStringList('notes');
+    if (notesJson != null) {
+      setState(() {
+        _notes = notesJson.map((note) => Map<String, String>.from(jsonDecode(note))).toList();
+      });
+    }
+  }
+
+  void _saveNotes() async {
+    final prefs = await SharedPreferences.getInstance();
+    final notesJson = _notes.map((note) => jsonEncode(note)).toList();
+    prefs.setStringList('notes', notesJson);
+  }
 
   void _addNote() {
     TextEditingController titleController = TextEditingController();
@@ -70,6 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     'note': noteController.text,
                   });
                 });
+                _saveNotes();
                 Navigator.pop(context);
               },
               child: Text('Save'),
@@ -78,6 +102,13 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
     );
+  }
+
+  void _removeNote(int index) {
+    setState(() {
+      _notes.removeAt(index);
+    });
+    _saveNotes();
   }
 
   @override
@@ -92,32 +123,51 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Column(
-              children: _notes
-                  .map(
-                    (note) => Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.all(8),
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${note['title'] ?? 'No Title'}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+              children: _notes.asMap().entries.map(
+                    (entry) {
+                  final index = entry.key;
+                  final note = entry.value;
+
+                  return Dismissible(
+                    key: Key(index.toString()),
+                    onDismissed: (direction) {
+                      _removeNote(index);
+                    },
+                    background: Container(
+                      color: Colors.red,
+                      child: Icon(Icons.delete, color: Colors.white),
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(8),
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      SizedBox(height: 5),
-                      Text(note['note'] ?? ''),
-                    ],
-                  ),
-                ),
-              )
-                  .toList(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            note['title'] ?? 'No title',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            note['note'] ?? '',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
             ),
           ],
         ),
@@ -125,7 +175,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _addNote,
         tooltip: 'Add note',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.note_add),
       ),
     );
   }
