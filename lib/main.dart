1d791e71c4ff2d47057fcc569957eb1ee232575a
Generekalipsa <djgamez1222@gmail.com>
2024-01-30 15:01:53 +0100

    Added option to edit notes and information button in Settings screen. Minor layout changes.

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
     _loadNotes();
   }
 
-  void _emptyFunction(){}
-
   void _loadNotes() async {
     final prefs = await SharedPreferences.getInstance();
     final notesJson = prefs.getStringList('notes');
@@ -64,15 +62,15 @@ class _MyHomePageState extends State<MyHomePage> {
     prefs.setStringList('notes', notesJson);
   }
 
-  void _addNote() {
-    TextEditingController titleController = TextEditingController();
-    TextEditingController noteController = TextEditingController();
+  void _addNoteOrEditNote({Map<String, String>? noteToEdit}) {
+    TextEditingController titleController = TextEditingController(text: noteToEdit?['title'] ?? '');
+    TextEditingController noteController = TextEditingController(text: noteToEdit?['note'] ?? '');
 
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Add Note'),
+          title: Text(noteToEdit == null ? 'Add Note' : 'Edit Note'),
           content: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -80,9 +78,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 controller: titleController,
                 decoration: InputDecoration(labelText: 'Enter note title'),
               ),
-              TextField(
-                controller: noteController,
-                decoration: InputDecoration(labelText: 'Enter your note'),
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: 200.0,
+                ),
+                child: TextField(
+                  controller: noteController,
+                  expands: true,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(labelText: 'Enter your note'),
+                ),
               ),
             ],
           ),
@@ -96,15 +102,23 @@ class _MyHomePageState extends State<MyHomePage> {
             TextButton(
               onPressed: () {
                 setState(() {
-                  _notes.add({
-                    'title': titleController.text,
-                    'note': noteController.text,
-                  });
+                  if (noteToEdit == null) {
+                    _notes.add({
+                      'title': titleController.text,
+                      'note': noteController.text,
+                    });
+                  } else {
+                    final index = _notes.indexOf(noteToEdit);
+                    if (index != -1) {
+                      _notes[index]['title'] = titleController.text;
+                      _notes[index]['note'] = noteController.text;
+                    }
+                  }
                 });
                 _saveNotes();
                 Navigator.pop(context);
               },
-              child: Text('Save'),
+              child: Text(noteToEdit == null ? 'Save' : 'Update'),
             ),
           ],
         );
@@ -126,91 +140,94 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         actions: [
           IconButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SettingsScreen())
-                );
-              },
-              tooltip: "settings",
-              icon: const Icon(Icons.settings))
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              );
+            },
+            tooltip: "settings",
+            icon: const Icon(Icons.settings),
+          )
         ],
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Column(
-              children: _notes.asMap().entries.map(
-                    (entry) {
-                  final index = entry.key;
-                  final note = entry.value;
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                children: _notes.asMap().entries.map(
+                      (entry) {
+                    final index = entry.key;
+                    final note = entry.value;
 
-                  return Dismissible(
-                    key: Key(index.toString()),
-                    onDismissed: (direction) {
-                      _removeNote(index);
-                    },
-                    background: Container(
-                      color: Colors.red,
-                      child: Icon(Icons.delete, color: Colors.white),
-                    ),
-                    child: Container(
-                      width: double.infinity,
-                      margin: EdgeInsets.all(8),
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10.0),
+                    return GestureDetector(
+                      onLongPress: () {
+                        _addNoteOrEditNote(noteToEdit: note);
+                      },
+                      child: Builder(
+                        builder: (context) {
+                          try {
+                            return Dismissible(
+                              key: Key(index.toString()),
+                              onDismissed: (direction) {
+                                _removeNote(index);
+                              },
+                              background: Container(
+                                color: Colors.red,
+                                child: Icon(Icons.delete, color: Colors.white),
+                              ),
+                              child: Container(
+                                width: double.infinity,
+                                margin: EdgeInsets.all(8),
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      note['title'] ?? 'No title',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      note['note'] ?? '',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          } catch (e) {
+                            print('Error: $e');
+                            return SizedBox.shrink();
+                          }
+                        },
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            note['title'] ?? 'No title',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            note['note'] ?? '',
-                            style: TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ).toList(),
-            ),
-          ],
+                    );
+                  },
+                ).toList(),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton:
-
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: _emptyFunction,
-              tooltip: 'Add list',
-              child: const Icon(Icons.checklist),
-            ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: _addNote,
-              tooltip: 'Add note',
-              child: const Icon(Icons.note_add),
-            ),
-          ],
-        )
-
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addNoteOrEditNote();
+        },
+        tooltip: 'Add',
+        child: const Icon(Icons.note_add),
+      ),
     );
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -5,8 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import 'theme_provider.dart';
 
-
-
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
@@ -15,6 +13,37 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('About application'),
+                    content: Text(
+                            'This application was created as an academic project in Flutter.\n'
+                            'It is an open-source project which anyone is free to use, modify, distribute and share under the BSD-style license.\n'
+                            '\nLibraries used in a project are:\n\n'
+                            '-provider\nhttps://github.com/rrousselGit/provider\n\n'
+                            '-shared preferences\nhttps://github.com/flutter/packages/tree/main/packages/shared_preferences/shared_preferences\n'
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Close'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            tooltip: "About app",
+            icon: const Icon(Icons.info),
+          )
+        ],
       ),
       body: Center(
           child: Container(
@@ -23,28 +52,34 @@ class SettingsScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Theme switch',
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Change theme',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Consumer<ThemeProvider>(
+                      builder: (context, themeProvider, child) {
+                        return Row(
+                          children: [
+                            Icon(Icons.light_mode),
+                            Switch(
+                              value: themeProvider.themeData == darkMode,
+                              onChanged: (value) {
+                                themeProvider.toggleTheme();
+                              },
+                            ),
+                            Icon(Icons.dark_mode),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                Consumer<ThemeProvider>(
-                  builder: (context, themeProvider, child) {
-                    return Row(
-                      children: [
-                        Icon(Icons.light_mode),
-                        Switch(
-                          value: themeProvider.themeData == darkMode,
-                          onChanged: (value) {
-                            themeProvider.toggleTheme();
-                          },
-                        ),
-                        Icon(Icons.dark_mode),
-                      ],
-                    );
-                  },
-                ),
+                SizedBox(height: 8),
               ],
             ),
           ),
